ad629b8fc7607a14f1b4198f5232fe3397b2a46f
theMackabu <theMackabu@gmail.com>
2023-10-22 00:25:32 -0700

    revert to bold text similar to golang version

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,14 @@ fn main() {
     let desktop = home_dir().join("Desktop");
     let screenshots = find_files(&desktop, "Screenshot");
     let recordings = find_files(&desktop, "Screen Recording");
-    let message = |name: &str, files: &Vec<PathBuf>| format!("moved {} {} to the {name} folder.", files.len(), ternary!(files.len() > 1, "files", "file").cyan()).bright_blue();
+    let message = |name: &str, files: &Vec<PathBuf>| {
+        format!("moved {} {} to the {name} folder.", files.len(), ternary!(files.len() > 1, "files", "file").cyan().bold())
+            .bright_blue()
+            .bold()
+    };
 
     if screenshots.is_empty() && recordings.is_empty() {
-        println!("{}", "no new files found to organize".yellow())
+        println!("{}", "no new files found to organize".yellow().bold())
     }
 
     if !screenshots.is_empty() {
@@ -23,7 +27,7 @@ fn main() {
             let screenshot = screenshots_dir.join(file_name);
             match move_file(&file, &screenshot) {
                 Ok(name) => println!("{} {}", "+".bright_green(), format!("{name}").white()),
-                Err(err) => eprintln!("{}", format!("error moving file: {:?}", err).red()),
+                Err(err) => eprintln!("{}", format!("error moving file: {:?}", err).bright_red().bold()),
             }
         }
     }
@@ -37,7 +41,7 @@ fn main() {
             let recording = recordings_dir.join(file_name);
             match move_file(&file, &recording) {
                 Ok(name) => println!("{} {}", "+".bright_green(), format!("{name}").white()),
-                Err(err) => eprintln!("{}", format!("error moving file: {:?}", err).red()),
+                Err(err) => eprintln!("{}", format!("error moving file: {:?}", err).bright_red().bold()),
             }
         }
     }
